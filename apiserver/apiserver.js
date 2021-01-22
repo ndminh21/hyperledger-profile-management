af13aab6dede5d3ea979e4fd6833c96aac92531d
minhnd <minh.nd@teko.vn>
2021-01-22 14:46:11 +0700

    SignOut flow

--- apiserver/apiserver.js
+++ apiserver/apiserver.js
@@ -73,6 +73,67 @@ app.post('/api/org/login', async function (req, res) {
     }
 });
 
+app.post('/api/v2/org/login', async function (req, res) {
+    try {
+        const ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
+        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const identity = await wallet.get('appUser');
+        if (!identity) {
+            console.log('An identity for the user "appUser" does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            return;
+        }
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('fabcar');
+        // Evaluate the specified transaction.
+        // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
+        // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
+        console.log('Params', req.body)
+        const result = await contract.evaluateTransaction('IsValidOrg', req.body.username, req.body.password);
+        console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
+        // res.status(200).json({response: result.toString()});
+        // console.log(JSON.parse(result).data)
+        if (result.toString() == "true") {
+            // res.redirect('/api/allprofiles')
+            res.send({
+                "userName": req.body.username,
+                "isValid": true
+            })
+        } else {
+            // res.render('org/login', { message: "Invalid user" })
+            res.send({
+                "userName": req.body.username,
+                "isValid": false,
+                "errorMessage": "Invalid user"
+            })
+        }
+
+    } catch (error) {
+        console.error(`Failed to evaluate transaction: ${error}`);
+        // res.status(500).json({error: error});
+        // process.exit(1);
+        // res.render('login');
+        res.send({
+            "userName": req.body.username,
+            "isValid": false,
+            "errorMessage": "Invalid user"
+        })
+    }
+});
+
 app.post('/api/org/register', async function (req, res) {
     try {
         const ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
@@ -219,7 +280,7 @@ app.get('/api/', function (req, res) {
 });
 
 app.get('/api/login', function (req, res) {
-    res.render('login');
+    res.render('login-v2');
 });
 
 app.get('/api/register', function (req, res) {
@@ -279,6 +340,60 @@ app.post('/api/login', async function (req, res) {
         // res.status(500).json({error: error});
         // process.exit(1);
         res.render('login');
+    }
+});
+
+
+app.post('/api/v2/login', async function (req, res) {
+    try {
+        const ccpPath = path.resolve(__dirname, '..', '..', 'test-network', 'organizations', 'peerOrganizations', 'org1.example.com', 'connection-org1.json');
+        const ccp = JSON.parse(fs.readFileSync(ccpPath, 'utf8'));
+        // Create a new file system based wallet for managing identities.
+        const walletPath = path.join(process.cwd(), 'wallet');
+        const wallet = await Wallets.newFileSystemWallet(walletPath);
+        console.log(`Wallet path: ${walletPath}`);
+
+        // Check to see if we've already enrolled the user.
+        const identity = await wallet.get('appUser');
+        if (!identity) {
+            console.log('An identity for the user "appUser" does not exist in the wallet');
+            console.log('Run the registerUser.js application before retrying');
+            return;
+        }
+        // Create a new gateway for connecting to our peer node.
+        const gateway = new Gateway();
+        await gateway.connect(ccp, { wallet, identity: 'appUser', discovery: { enabled: true, asLocalhost: true } });
+
+        // Get the network (channel) our contract is deployed to.
+        const network = await gateway.getNetwork('mychannel');
+
+        // Get the contract from the network.
+        const contract = network.getContract('fabcar');
+        // Evaluate the specified transaction.
+        // queryCar transaction - requires 1 argument, ex: ('queryCar', 'CAR4')
+        // queryAllCars transaction - requires no arguments, ex: ('queryAllCars')
+        const result = await contract.evaluateTransaction('isValidUser', req.body.username, req.body.password);
+        console.log(`Transaction has been evaluated, result is: ${result.toString()}`);
+        if (result.toString() == "true") {
+            // res.redirect('/api/allprofiles')
+            res.send({
+                "userName": req.body.username,
+                "isValid": true
+            })
+        } else {
+            res.send({
+                "userName": req.body.username,
+                "isValid": false,
+                "errorMessage": "Invalid user"
+            })
+        }
+
+    } catch (error) {
+        res.send({
+            "userName": req.body.username,
+            "isValid": false,
+            "errorMessage": "Invalid user"
+        })
     }
 });
 
